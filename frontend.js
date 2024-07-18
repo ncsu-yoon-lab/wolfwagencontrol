6ee057e1fa68b4f6d49647170e3808429d948084
adrhito <130334763+adrhito@users.noreply.github.com>
2024-07-18 10:29:49 -0400

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -1,4 +1,4 @@
-const rosApiUrl = 'http://127.0.0.1:5000'; // Replace with your ROS API endpoint
+const rosApiUrl = 'http://10.138.194.205:5000'; // Replace with your ROS API endpoint
 
 async function sendCommand(throttle, steering) {
     const command = {
